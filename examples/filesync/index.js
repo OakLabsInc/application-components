3c62be2a47ca295a916a05d0f7b2a8be830d5502
Flynn Joffray <nucleardreamer@gmail.com>
2018-11-07 12:40:14 +0000

    adding node examples

---
+++ examples/filesync/index.js
@@ -0,0 +1,18 @@
+const { join } = require('path')
+const QuickgRPC = require('quick-grpc')
+const _ = require('lodash')
+
+async function go () {
+  const { filesync } = await new QuickgRPC({
+    host: 'localhost:9102',
+    basePath: join(__dirname, '..', '..', 'filesync')
+  })
+
+  let fs = await filesync()
+  fs.info(undefined, function (err, data) {
+    if (err) throw err
+    console.log(JSON.stringify(data, null, 2))
+  })
+}
+
+go()
